473da28b0929acd02fd3e2d83b8a8857f5556199
Akif Imran <raiden3322@gmail.com>
2026-04-16 15:12:46 +0500

    Add: mergeSort - submission-0

---
+++ Data Structures & Algorithms/mergeSort/submission-0.js
@@ -0,0 +1,62 @@
+/** Pair class to store key-value pairs */
+// class Pair {
+//   /**
+//    * @param {number} key The key to be stored in the pair
+//    * @param {string} value The value to be stored in the pair
+//    */
+//   constructor(key, value) {
+//       this.key = key;
+//       this.value = value;
+//   }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[]}
+     */
+    mergeSort(pairs) {
+        const mergeSortList = function (list, L, R) {
+            if (L === R) return list;
+
+            const M = Math.floor((L + R) / 2);
+            mergeSortList(list, L, M);
+            mergeSortList(list, M + 1, R);
+            return merge(list, L, M, R);
+        };
+
+        const merge = function (list, L, M, R) {
+            const left = list.slice(L, M + 1);
+            const right = list.slice(M + 1, R + 1);
+
+            let i = L,
+                li = 0,
+                ri = 0;
+
+            while (li < left.length && ri < right.length) {
+                if (left[li].key <= right[ri].key) {
+                    list[i] = left[li];
+                    li += 1;
+                } else {
+                    list[i] = right[ri];
+                    ri += 1;
+                }
+                i += 1;
+            }
+
+            while (li < left.length) {
+                list[i] = left[li];
+                li += 1;
+                i += 1;
+            }
+
+            while (ri < right.length) {
+                list[i] = right[ri];
+                ri += 1;
+                i += 1;
+            }
+            return list;
+        };
+        console.log(pairs[0], pairs.length, pairs[0].key);
+        return mergeSortList(pairs, 0, pairs.length - 1);
+    }
+}
