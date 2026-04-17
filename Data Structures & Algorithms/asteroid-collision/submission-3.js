9c76de4a6fa8fe00c7e0f57be197c72a5c92bb8b
Akif Imran <raiden3322@gmail.com>
2026-04-17 11:24:44 +0500

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+        const res = [asteroids[0]];
+
+        for (let i = 1; i < asteroids.length; i++) {
+            let incomming = asteroids[i];
+            if (incomming > 0) {
+                res.push(incomming);
+            } else {
+                if (res.length === 0 || res[res.length - 1] < 0) res.push(incomming);
+                else {
+                    let push = true;
+                    let destroyed = false;
+                    do {
+                        let diff = incomming + res[res.length - 1];
+                        console.log(incomming, res[res.length - 1], diff);
+                        if (diff < 0) {
+                            res.pop();
+                        } else if (diff > 0) {
+                            push = false;
+                        } else {
+                            res.pop();
+                            push = false;
+                        }
+                    } while (res[res.length - 1] > 0 && push);
+                    if (push) {
+                        res.push(incomming);
+                    }
+                }
+            }
+            console.log("res: ", res);
+        }
+        return res;
+    }
+}
