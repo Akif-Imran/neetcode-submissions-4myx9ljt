933a05be578c7e688d9205ca07f2ec40c8d8361d
Akif Imran <raiden3322@gmail.com>
2026-04-17 12:08:16 +0500

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const n = temperatures.length;
+        const res = [];
+        const output = new Array(n).fill(0);
+
+        for (let i = n - 1; i >= 0; i--) {
+            while (res.length > 0 && res[res.length - 1].value <= temperatures[i]) {
+                res.pop();
+            }
+            if (res.length > 0) output[i] = res[res.length - 1].index - i;
+            else output[i] = 0;
+            res.push({ value: temperatures[i], index: i });
+        }
+        return output;
+    }
+    //[(40,5),(36,3),(30,2)]
+    //Time: 
+    //Space: O(n);
+}
