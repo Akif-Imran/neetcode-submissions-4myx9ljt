9463e65e516f601a0cb18fdc4f6d42c32105ddef
Akif Imran <raiden3322@gmail.com>
2026-05-06 12:28:02 +0500

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.js
@@ -0,0 +1,51 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, { prev: 1001, arr: [] });
+        }
+        this.keyStore.get(key).arr.push({ val: value, timestamp });
+        this.keyStore.get(key).prev = Math.min(this.keyStore.get(key).prev, timestamp);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if (!this.keyStore.has(key)) return "";
+
+        const { prev, arr } = this.keyStore.get(key);
+        console.log(prev, arr);
+        if (timestamp < prev) return "";
+
+        let l = 0;
+        let r = arr.length - 1;
+        let res = 0;
+
+        while (l <= r) {
+            const mid = l + Math.floor((r - l) / 2);
+            if (timestamp === arr[mid].timestamp) return arr[mid].val;
+            else if (timestamp > arr[mid].timestamp) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        res = l;
+        if (l >= arr.length) {
+            res = arr.length - 1;
+        }
+        return arr[res].val;
+    }
+}
