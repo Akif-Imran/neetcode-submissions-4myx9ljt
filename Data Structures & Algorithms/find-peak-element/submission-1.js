89b7d5d1d7f44020e5ae70f7a6cdaee339eda610
Akif Imran <raiden3322@gmail.com>
2026-05-12 22:02:53 +0500

    Add: find-peak-element - submission-1

---
+++ Data Structures & Algorithms/find-peak-element/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findPeakElement(nums) {
+        const n = nums.length;
+        if (n === 1) return 0;
+
+        // if (n > 1 && nums[0] > nums[1]) return 0;
+        // if (n > 1 && nums[n - 1] > nums[n - 2]) return n - 1;
+        //if element is at 0 ...,[0 it's greater than the left part
+        //only check right index
+        //if element is at n-1 ...[...,0] it's greater than the right part
+        //only check left index
+
+        let l = 0;
+        let r = n - 1;
+        while (l < r) {
+            const mid = l + Math.floor((r - l) / 2);
+
+            if (mid === 0 && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid === n - 1 && nums[mid - 1] > nums[mid]) {
+                return mid;
+            }
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                return mid;
+            } else if (nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else if (nums[mid] < nums[mid - 1]) {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+}
