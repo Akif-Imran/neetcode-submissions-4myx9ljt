2a2b002ccc829985157ef124e7f51d538c1079b7
Akif Imran <raiden3322@gmail.com>
2026-05-18 14:01:45 +0500

    Add: missing-element-in-sorted-array - submission-2

---
+++ Data Structures & Algorithms/missing-element-in-sorted-array/submission-2.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    missingElement(nums, k) {
+        //[4,5,6,7,8,9,10]
+        //7-4 = 3 // 9 - 4 = 5 - 2(index) = 3
+        //l + 3 === mid?
+        const missing_values = nums.at(-1) - (nums[0] - 1) - nums.length;
+        if (missing_values < k) {
+            k -= missing_values;
+            return nums.at(-1) + k;
+        }
+
+        const n = nums.length;
+        let l = 0;
+        let r = n - 1;
+        let res = 0;
+
+        while (l < r) {
+            const mid = l + Math.floor((r - l) / 2);
+
+            let missing = nums[mid] - nums[l]; //take -1 for the actual nums[l]
+            missing = missing - (mid - l);
+            if (nums[l + 1] === nums[r]) {
+                res = nums[l] + k;
+                break;
+            } else if (missing >= k) {
+                r = mid;
+            } else {
+                k -= missing;
+                l = mid;
+            }
+        }
+        return res;
+    }
+}
